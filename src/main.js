012b0fc626b1683683e862c662072301a4341b43
GRiT <alex-kashin-02@mail.ru>
2023-02-10 14:59:44 +0300

    nothing work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+new Vue({
+    render: h => h(App),
+  }).$mount('#app')
+
+
+/*import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app')*/
